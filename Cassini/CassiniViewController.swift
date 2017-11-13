b9ec09b3cbd3b021526d92cee1a89a7783d580e5
Glafira Privalova <glafira.privalova@metropolia.fi>
2017-11-14 00:29:20 +0200

    Demo project to text dispatch queue when getting something from network that might take a while

---
+++ Cassini/CassiniViewController.swift
@@ -0,0 +1,74 @@
+//
+//  CassiniViewController.swift
+//  Cassini
+//
+//  Created by Glaphi on 31/10/2017.
+//  Copyright © 2017 glaphi. All rights reserved.
+//
+
+import UIKit
+
+class CassiniViewController: UIViewController, UISplitViewControllerDelegate {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.splitViewController?.delegate = self
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let url = DemoURL.NASA[segue.identifier ?? ""] {
+            if let imageVC = (segue.destination.contents as? ImageViewController) {
+                imageVC.imageURL = url
+                imageVC.title = (sender as? UIButton)?.currentTitle
+            }
+        }
+    }
+    
+    func splitViewController(_ splitViewController: UISplitViewController,
+                             collapseSecondary secondaryViewController: UIViewController,
+                             onto primaryViewController: UIViewController) -> Bool {
+        if primaryViewController.contents == self {
+            if let ivc = secondaryViewController.contents as? ImageViewController, ivc.imageURL == nil {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+
+extension UIViewController {
+    var contents: UIViewController {
+        if let navCon = self as? UINavigationController {
+            return navCon.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
+}
+
+
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
